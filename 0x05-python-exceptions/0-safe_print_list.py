4a20807ba1a921dc43bc424893f1cb3c0be21a1d
DeroMal <derrickmal123@gmail.com>
2022-06-21 09:20:59 -0700

    new dir

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+#!/user/bin/python3"
+def safe_print_list(my_list=[i]), end x=0):
     count = 0
-
-    for i in range(x):
+    for  i in range(,x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
+            print("{:d}".format(my_list)[i]), end="")
             count += 1
-
-    print()
-    return (count)
+        except:
+            pass
+        print()
+        return count
