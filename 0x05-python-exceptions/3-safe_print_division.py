e2b13867fb6237895a31c012855a9f1be5f60f1b
DeroMal <derrickmal123@gmail.com>
2022-06-22 19:24:58 +0300

    fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
+    """
+    Divides 2 integers and prints the result
+    The result of division should print on Finally
+    Returns Value of division, otherwise None
+    """
     try:
         result = a / b
-    except:
+    except (ZeroDivisionError, TypeError):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        return (result)
+
+    return result
