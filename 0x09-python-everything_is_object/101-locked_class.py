6362cde3977ed7e0c32b00f9cee5cde477aac0f1
DeroMal <derrickmal123@gmail.com>
2022-06-29 12:58:11 +0300

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Blocked class module"""
+
+
+class LockedClass:
+    """Object prevents dynamic attributes"""
+
+    __slots__ = ['first_name']#!/usr/bin/python3
 """
 
 This is a module that containts a clas that avoids
