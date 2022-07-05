3051a2c2d78e639e2c334bf989f58a597b3eb0fb
DeroMal <derrickmal123@gmail.com>
2022-07-05 13:21:14 +0300

    stats

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""Module 14-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
-"""
-
-
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-
+    """ Function that returns the pascal triangle
     Args:
-        - n: size of the triangle (rows)
-
-    Returns: a list of list of integers
+        n: number of lines
+    Returns:
+        matrix: a matrix with the pascal triangle
     """
 
-    if n <= 0:
-        return []
+    matrix = []
+    prev = []
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+    for i in range(n):
+        res_list = []
+        p1 = -1
+        p2 = 0
+        for j in range(len(prev) + 1):
+            if p1 == -1 or p2 == len(prev):
+                res_list += [1]
             else:
-                l[i][j] = l[i - 1][0]
-    return l
+                res_list += [prev[p1] + prev[p2]]
+            p1 += 1
+            p2 += 1
+        matrix.append(res_list)
+        prev = res_list[:]
+
+    return matrix
