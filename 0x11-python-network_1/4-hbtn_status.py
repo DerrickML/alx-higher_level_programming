f1d8ad70ef8946bb36d58b702fa00af378aa6e08
DeroMal <derrickmal123@gmail.com>
2022-09-23 23:45:17 +0300

    ds

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-
+import requests
 
 if __name__ == "__main__":
-    import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
 
