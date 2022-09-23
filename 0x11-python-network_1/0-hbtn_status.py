ae697c804749301bb8d62865bc3ccdd6225914f5
DeroMal <derrickmal123@gmail.com>
2022-09-23 23:39:04 +0300

    ds

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-'''
-Python script that fetches https://intranet.hbtn.io/status 
-'''
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    alx_url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(alx_url) as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
